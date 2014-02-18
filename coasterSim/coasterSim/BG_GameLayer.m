50f312032b276465601958e568c99e0f8fcfa229
Joe90 <joper90@gmail.com>
2014-02-18 10:22:53 +0000

    Added the BG

--- coasterSim/coasterSim/BG_GameLayer.m
+++ coasterSim/coasterSim/BG_GameLayer.m
@@ -21,6 +21,14 @@
         //No handling enabled for this layer
         self.touchEnabled = NO;
 
+        //Lets add a bg image.
+        //Set the best mode?
+        [CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGB565];
+        CCSprite *background = [CCSprite spriteWithFile:@"coasterBG.png"];
+        background.anchorPoint = ccp(0,0);
+        [self addChild:background];
+        [CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_Default];
+        
 	}
 	return self;
 }
